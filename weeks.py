6a620dfe5636d05a0ff9e6736588a8593065fe40
Jacqueline Nguyen <beuyenj@gmail.com>
2021-03-22 16:18:40 -0700

    Changed MIN_DATE to 06/21/2019

--- weeks.py
+++ weeks.py
@@ -1,17 +1,19 @@
 # Jacqueline Nguyen
+# getweeks will return a dictionary with all the date ranges and fridays from that date range
 import pandas as pd
 import datetime
 from datetime import date
 
 today = date.today()
+MIN_DATE = '6/21/2019'
 
 def allfridays(today):
-    return pd.date_range(start='6/21/2018', end=today,
+    return pd.date_range(start=MIN_DATE, end=today,
         freq='W-FRI').strftime('%Y-%m-%d').tolist()
 
 def timerange(friday) :
     fri = datetime.datetime(int(friday[:4]), int(friday[5:7]), int(friday[-2:]))
-    thursday = fri + datetime.timedelta( (3-today.weekday()) % 7 )
+    thursday = fri + datetime.timedelta( (3-fri.weekday()) % 7 )
     fri_str = fri.strftime('%b %d, %Y')
     thur_str = thursday.strftime('%b %d, %Y')
     s = f'{fri_str} - {thur_str}'
@@ -25,8 +27,10 @@ def getweeks(today) :
 
 '''
 last = allfridays(today)[-1]
+print(last)
 print(timerange(last))
-#print(formatDate(today))
 '''
-
-print(getweeks(today))
+'''
+for k,v in getweeks(today).items() :
+    print(f'{k}: {v}')
+'''
