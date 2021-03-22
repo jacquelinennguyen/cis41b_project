7900b9b5d0039bf1cfdff325650dec02e08b39b1
Jacqueline Nguyen <beuyenj@gmail.com>
2021-03-22 16:34:01 -0700

    Web scraped min date and max date from rollingstones website

--- weeks.py
+++ weeks.py
@@ -3,12 +3,18 @@
 import pandas as pd
 import datetime
 from datetime import date
+import requests
+from bs4 import BeautifulSoup
 
-today = date.today()
-MIN_DATE = '6/21/2019'
+response = requests.get(f"https://www.rollingstone.com/charts/songs/")
+soup = BeautifulSoup(response.content, "lxml")
 
-def allfridays(today):
-    return pd.date_range(start=MIN_DATE, end=today,
+for tag in soup.select("input#rs-charts-datepicker") :
+    MIN_DATE = tag['data-min-date']
+    MAX_DATE = tag['data-max-date']
+
+def allfridays(max_date):
+    return pd.date_range(start=MIN_DATE, end=max_date,
         freq='W-FRI').strftime('%Y-%m-%d').tolist()
 
 def timerange(friday) :
@@ -19,18 +25,18 @@ def timerange(friday) :
     s = f'{fri_str} - {thur_str}'
     return s
 
-def getweeks(today) :
+def getweeks(max_date) :
     d = {}
-    for friday in allfridays(today) :
+    for friday in allfridays(max_date) :
         d[timerange(friday)] = friday
     return d
 
 '''
-last = allfridays(today)[-1]
+last = allfridays(MAX_DATE)[-1]
 print(last)
 print(timerange(last))
 '''
 '''
-for k,v in getweeks(today).items() :
+for k,v in getweeks(MAX_DATE).items() :
     print(f'{k}: {v}')
 '''
